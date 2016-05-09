97edaa0e2dd5c541524932edcab129c93778b818
Terry Brown <terry_n_brown@yahoo.com>
2016-05-09 13:53:01 -0500

    plot with lines

--- stream_temp.R
+++ stream_temp.R
@@ -1,4 +1,4 @@
 
 d <- read.csv("1998_Jackson_Cr_Lower_corrected.csv")
 d$datetime <- strptime(paste(d$Date, d$Time), "%m/%d/%Y %H:%M:%S")
-plot(d$datetime, d$Temperature)
+plot(d$datetime, d$Temperature, type='l')
